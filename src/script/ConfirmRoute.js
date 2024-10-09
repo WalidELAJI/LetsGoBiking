e67524e233499956e5d047ed44a3ee41dca2ea65
walidELAJI <walid.el-aji@etu.unice.fr>
2024-10-09 16:45:57 +0200

    Yasmine 7alawa

---
+++ src/script/ConfirmRoute.js
@@ -0,0 +1,12 @@
+function confirmRoute() {
+    const origin = document.getElementById('origin').value;
+    const destination = document.getElementById('destination').value;
+    document.getElementById('notification-message').textContent = `Itinéraire confirmé de ${origin} à ${destination}`;
+
+    const notification = document.getElementById('custom-notification');
+    notification.classList.remove('hidden');
+
+    setTimeout(() => {
+        notification.classList.add('hidden');
+    }, 2000);
+}
