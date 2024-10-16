a5abb26778b7dfd5680c32c16e613dae37b3f90b
walidELAJI <walid.el-aji@etu.unice.fr>
2024-10-16 15:47:22 +0200

    l'ajout de la carte

--- src/script/ConfirmRoute.js
+++ src/script/ConfirmRoute.js
@@ -10,3 +10,9 @@ function confirmRoute() {
         notification.classList.add('hidden');
     }, 2000);
 }
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
