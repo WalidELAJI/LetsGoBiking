853c61e741441506298acd2e479661bfd54c479e
walidELAJI <walid.el-aji@etu.unice.fr>
2024-10-23 15:18:20 +0200

    ajout de Geocoder

--- src/script/ConfirmRoute.js
+++ src/script/ConfirmRoute.js
@@ -16,3 +16,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+L.Control.geocoder().addTo(map);
